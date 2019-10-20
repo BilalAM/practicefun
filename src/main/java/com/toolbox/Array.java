a2257900e9327ad61a30d24d19eb4aebb113053e
BilalAM <bilal.asif.97@gmail.com>
2019-10-20 22:38:49 +0500

    added a generic grow method

--- src/main/java/com/toolbox/Array.java
+++ src/main/java/com/toolbox/Array.java
@@ -1,4 +1,21 @@
 package com.toolbox;
 
 public class Array {
+
+    /**
+     * Grows the existing cache array by the specified growth limit .
+     *
+     * @param arrayToGrow : The array to which to grow .
+     * @param growth      : How many elements by which it should grow .
+     * @return : A new array that has the elements of the old array
+     * with the size specified .
+     */
+
+    public static <T> T[] grow(T[] arrayToGrow, int growth) {
+        T[] newArray = (T[]) new Object[arrayToGrow.length + growth];
+        for (int indexOfOldArray = 0; indexOfOldArray < arrayToGrow.length; indexOfOldArray++) {
+            newArray[indexOfOldArray] = arrayToGrow[indexOfOldArray];
+        }
+        return newArray;
+    }
 }
